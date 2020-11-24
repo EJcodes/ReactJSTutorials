3b9e89fa95feeb779626f8cd11b80e543aaced77
EJcodes <IHMDemon.exe@gmail.com>
2020-11-23 19:08:48 -0500

    learning a bit more about class components in react

--- ReactTheCompleteGuide/react-complete-guide/src/App.js
+++ ReactTheCompleteGuide/react-complete-guide/src/App.js
@@ -3,14 +3,22 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state={
+    persons:[
+      { name: 'Max', age: 28 },
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 29}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react App</h1>
         <p>This is really working!!!</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Stehphanie" age="26"/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     
     );
