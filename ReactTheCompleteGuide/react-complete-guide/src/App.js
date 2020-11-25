3e6b0f574954037b0a964e5044eb4da4d3df531c
EJcodes <IHMDemon.exe@gmail.com>
2020-11-25 17:48:42 -0500

    messing around with the bind keyword

--- ReactTheCompleteGuide/react-complete-guide/src/App.js
+++ ReactTheCompleteGuide/react-complete-guide/src/App.js
@@ -11,12 +11,12 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('was clicked!');
     // Dont do this: this.state.persons[0].name="Evan";
     this.setState( {
       persons: [
-        { name: 'Evan', age: 28 },
+        { name: newName, age: 28 },
         {name: 'Manu', age: 29 },
         {name: 'Stephanie', age: 27 }
       ]
@@ -28,9 +28,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react App</h1>
         <p>This is really working!!!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, 'Evan')}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Evan')}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     
