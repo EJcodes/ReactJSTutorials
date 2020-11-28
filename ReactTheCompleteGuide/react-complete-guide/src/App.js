efdf6db799d799ee84cef053093bd22b84d3f5d7
EJcodes <IHMDemon.exe@gmail.com>
2020-11-28 11:32:10 -0500

    little example on how this 2 way binding is implemented

--- ReactTheCompleteGuide/react-complete-guide/src/App.js
+++ ReactTheCompleteGuide/react-complete-guide/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     // Dont do this: this.state.persons[0].name="Evan";
     this.setState( {
       persons: [
-        { name: 'Max', age: 28 },
+        { name: newName, age: 28 },
         {name: 'Manu', age: 29 },
         {name: 'Stephanie', age: 27 }
       ]
@@ -26,8 +26,8 @@ class App extends Component {
   nameChangeHandler = (event) =>{
     this.setState( {
       persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29},
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29},
         { name: 'Stephanie', age: 27}
       ]
     } )
@@ -40,7 +40,11 @@ class App extends Component {
         <p>This is really working!!!</p>
         <button onClick={this.switchNameHandler.bind(this, 'Evan')}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Evan')}/>
+        <Person name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} 
+        click={this.switchNameHandler.bind(this, 'Evan')}
+        changed={this.nameChangeHandler}
+        >My Hobbies: racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     
